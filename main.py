a055e8fe6a044efd6de85698c7d2e279bd61892e
masoud <shakoury60@gmail.com>
2023-10-02 19:10:14 +0330

    info put inside an iframe, using src of iframe in url

--- main.py
+++ main.py
@@ -1,40 +1,24 @@
-import time
-from bs4 import BeautifulSoup
-import requests
+
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
 
-driver = webdriver.Chrome()
+service = Service()
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+driver = webdriver.Chrome(service=service, options=options)
+wait = WebDriverWait(driver, 10)
 
-url = 'https://eproc.setadiran.ir/eproc/entry.do'
+# url0 = 'https://eproc.setadiran.ir/eproc/entry.do' 
+url = 'https://eproc.setadiran.ir/eproc/needs.do'
+
 driver.get(url)
-# wait = WebDriverWait(driver, 10)
-# driver.implicitly_wait(20)
-time.sleep(5)
 
-xpath = '//*@id="aList"/tbody'
-elem = driver.find_element(By.XPATH, xpath)
+xpath = '//*[@id="aList"]/tbody'
 
-print('elem is: ' , elem.text)
-# for e in elem:
-#     print(e.text)
-#     print('*'*20)
-# driver.close()
+# Wait until the element is visible
+element = wait.until(EC.visibility_of_element_located((By.XPATH, xpath)))
 
-
-# r = requests.get(url).content
-
-# # print(r)
-
-# soup = BeautifulSoup(r, 'html.parser')
-
-# rows = soup.find_all('tbody')
-
-# print(rows)
-# # print(soup.prettify())
-
-
-
+elem = element.find_elements(By.TAG_NAME, 'tr')
